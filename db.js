6047aaf4ebd0c874cfeef9fbf92d2a5d14cb954f
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2017-11-02 00:18:12 +0100

    experimenting with env variables

--- db.js
+++ db.js
@@ -12,6 +12,12 @@ exports.connect = function(done) {
     password: process.env.DB_USERNAME,
     database: process.env.DB_PASSWORD
   });
+  console.log({
+    host            : process.env.MYSQL_HOST,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_SECRET,
+    database        : process.env.MYSQL_DB
+ })
   done();
 }
 
