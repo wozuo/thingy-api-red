f10fcdbe2f9bc685fbfeddbdec7619ba25ec360c
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2017-12-15 22:42:33 +0100

    Forecast based on calendar is now working

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -4,13 +4,9 @@ var hapi = require('hapi');
 const db = require('../db');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-function getTodaysCalendarEvents(access_token) {
+function getRequest(url) {
   return new Promise(function(resolve, reject) {
-    var now = new Date();
-    var endOfDay = new Date();
-    endOfDay.setHours(23, 59, 59);
     var req = new XMLHttpRequest();
-    let url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=' + access_token + '&singleEvents=true&timeMin=' + now.toISOString() + '&timeMax=' + endOfDay.toISOString() + '';
     req.open('GET', url);
     req.onload = function() {
       if (req.status == 200 && req.readyState == 4) {
@@ -24,21 +20,14 @@ function getTodaysCalendarEvents(access_token) {
   });
 }
 
-function getCoordinatesFor(location) {
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
-    let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + process.env.GOOGLE_MAPS_API_KEY + '';
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status == 200 && req.readyState == 4) {
-        var jsonResp = JSON.parse(req.responseText);
-        resolve(jsonResp);
-      } else {
-        reject(Error(req.statusText));
-      }
-    };
-    req.send();
-  });
+function sortNumber(a,b) {
+    return a - b;
+}
+
+function removeDuplicates(myArr, prop) {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
 }
 
 function RecommendationController(){};
@@ -74,7 +63,7 @@ RecommendationController.prototype = (function() {
                           aocDesc: results[i].description
                         });
                       }
-                      if (closestData.humi >= 100) {
+                      if (closestData.humi >= 90) {
                         recommendations.push({
                           aocId: 5,
                           aocDesc: 'Raincoat (Rain)'
@@ -95,23 +84,96 @@ RecommendationController.prototype = (function() {
       })
     },
     getForecast: function getForecast(request, reply) {
-      getTodaysCalendarEvents(request.params.google_at).then(function(response) {
+      var now = new Date();
+      var endOfDay = new Date();
+      endOfDay.setHours(23, 59, 59);
+      var url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=' + request.params.google_at + '&singleEvents=true&timeMin=' + now.toISOString() + '&timeMax=' + endOfDay.toISOString() + '';
+      getRequest(url).then(function(response) {
         const items = response.items;
         let coordinatePromises = [];
+        var dateTimes = [];
         for(var i = 0; i < items.length; i++) {
           console.log('Item Location: ' + items[i].location);
-          coordinatePromises.push(getCoordinatesFor(items[i].location));
+          console.log('Item Event Time: ' + items[i].start.dateTime);
+          dateTimes.push(items[i].start.dateTime);
+          url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + items[i].location + '&key=' + process.env.GOOGLE_MAPS_API_KEY + '';
+          coordinatePromises.push(getRequest(url));
         }
         Promise.all(coordinatePromises).then((results) => {
           let coordinates = [];
           for(var i = 0; i < results.length; i++) {
-            //console.log('Location: ' + results[i].results[0].geometry.location.lat);
             var tempCoords = { lat: results[i].results[0].geometry.location.lat, lng: results[i].results[0].geometry.location.lng };
             coordinates.push(tempCoords);
           }
-          console.log('Lat: ' + coordinates[0].lat + ' long: ' + coordinates[0].lng);
-          
-          reply("TODO: implement forecast.").code(200);
+          let weatherPromises = [];
+          let unixTimes = [];
+          for(var i = 0; i < coordinates.length; i++) {
+            var accDateTime = new Date(dateTimes[i]);
+            if (accDateTime.getMinutes() < 30) {
+              accDateTime.setMinutes('00', '00', '00');
+            } else {
+              accDateTime.setMinutes('60', '00', '00');
+            }
+            var unixDateTimeValue = accDateTime.getTime()/1000;
+            unixTimes.push(unixDateTimeValue);
+            let url = 'https://api.darksky.net/forecast/f872bc130397ccd359931b25f820393c/' + coordinates[i].lat + ',' + coordinates[i].lng + '?time=' + unixDateTimeValue + '&units=si';
+            weatherPromises.push(getRequest(url));
+          }
+          Promise.all(weatherPromises).then((results) => {
+            let weatherData = [];
+            let temperatures = [];
+            let highestHumidity = 0;
+            for(var i = 0; i < results.length; i++) {
+              var index = -1;
+              for(var j = 0; j < results[i].hourly.data.length; j++) {
+                if(results[i].hourly.data[j].time == unixTimes[i]) { index = j }
+              }
+              console.log('Humidity: ' + results[i].hourly.data[index].humidity + " index: " + index + " time: " + results[i].hourly.data[index].time);
+              temperatures.push(results[i].hourly.data[index].temperature);
+              if (results[i].hourly.data[index].humidity > highestHumidity) {
+                highestHumidity = results[i].hourly.data[index].humidity;
+              }
+            }
+            db.get().query('SELECT user_id, username FROM users WHERE user_id = ?', request.params.user_id, function (error, results) {
+              if (error) {
+                console.log('Internal Server Error: ' + error)
+                reply('Internal Server Error:').code(500);
+              } else {
+                if (results.length == 1) {
+                  temperatures.sort(sortNumber);
+                  let highestTemp = temperatures[temperatures.length-1];
+                  let lowestTemp = temperatures[0];
+                  highestHumidity = highestHumidity * 100;
+                  console.log('Highest Temp: ' + highestTemp + ' lowest temp: ' + lowestTemp);
+                  db.get().query('SELECT clothes.aoc_id, clothes.description FROM clothes INNER JOIN users_clothes ON clothes.aoc_id = users_clothes.aoc_id WHERE (users_clothes.from_temp >= ' + lowestTemp + ' AND users_clothes.from_temp <= ' + highestTemp + ') OR (users_clothes.to_temp >= ' + lowestTemp + ' AND users_clothes.to_temp <= ' + highestTemp + ') OR (users_clothes.from_temp <= ' + lowestTemp + ' AND users_clothes.to_temp >= ' + highestTemp + ') AND users_clothes.user_id = ' + request.params.user_id + '', function (error, results) {
+                    if (error) {
+                      console.log('Internal Server Error: ' + error)
+                      reply('Internal Server Error:').code(500);
+                    } else {
+                      var recommendations = [];
+                      for (var i = 0; i < results.length; i++) {
+                        recommendations.push({
+                          aocId: results[i].aoc_id,
+                          aocDesc: results[i].description
+                        });
+                      }
+                      if (highestHumidity >= 80) {
+                        recommendations.push({
+                          aocId: 5,
+                          aocDesc: 'Raincoat (Rain)'
+                        })
+                      }
+                      // Remove duplicates in recommendations
+                      recommendations = removeDuplicates(recommendations, 'aocId');
+                      reply(recommendations).code(200);
+                    }
+                  })
+                } else {
+                  reply('User not found').code(404);
+                }
+              }
+            });
+          });
         });
       }, function(error) {
         console.error('Error', error);
